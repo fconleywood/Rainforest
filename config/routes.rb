81df1759921885def2651b86e57951fb6b0e9bac
frances conley wood <f.conleywood@gmail.com>
2016-01-29 16:07:55 -0500

    Creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   root 'products#index'
+
   resources :products
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # get 'users/new'
   #
